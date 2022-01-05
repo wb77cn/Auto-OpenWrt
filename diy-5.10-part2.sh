724814f3d1beb782cc96c1c407ce8832e3db47f3
wb77cn <77836424+wb77cn@users.noreply.github.com>
2022-01-05 12:30:09 +0800

    Update diy-5.10-part2.sh

--- diy-5.10-part2.sh
+++ diy-5.10-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generate
 
 ########### 更改大雕源码（可选）###########
 #sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=5.10/g' target/linux/x86/Makefile
